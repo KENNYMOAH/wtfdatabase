e1678db458ce72c75b05c184437450e1ecaa8840
Home 🏠 <Windoge8@compiling-error.local>
2015-12-22 21:04:34 +0800

    Initial Commit

---
+++ database/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  database
+//
+//  Created by lololol on 12/22/15.
+//  Copyright © 2015 Kenny Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
